99798fe5c62eb478a02041ec5bad233e8de79ce1
jungheeeun <jungheeeun272@gmail.com>
2023-04-28 11:28:09 +0900

    설명이해하기 & iquirer 예제실습

--- root/index.mjs
+++ root/index.mjs
@@ -1,5 +1,5 @@
 //! CLI 
-// npm install commander 모듈 설치
+// npm install commander.js 모듈 설치
 // git & pakahe.json 사용 및 활용 
 import { program } from 'commander';
 import inquirer from 'inquirer';
@@ -8,11 +8,12 @@ import fs from 'fs';
 const ROOT_TAG_CHOICES = ['<div id="root"></div>', ''];
 const DEFAULT_TITLE = 'My HTML File';
 
-program
+program // 옵션으로 -t , —title  , —no—root-tag 정의,  인자  <content> 정의하기
   .option('-t, --title <title>', 'title of HTML file')
   .option('--no-root-tag', 'exclude the root tag in HTML body')
   .arguments('<content>')
-  .action((content) => {
+  // 처음들어봄 -> program.action() 메소드에서 inquirer를 사용하여서 사용자로부터 파일명 입력받기     
+  .action((content) => { 
     inquirer
       .prompt([
         {
@@ -38,7 +39,13 @@ program
           </body>
         </html>
         `;
-
+//const dir = './result';: dir 상수에 './result'라는 문자열을 할당합니다.
+//이는 현재 작업 디렉토리의 하위 폴더인 result를 가리킵니다.
+// if (!fs.existsSync(dir)) {: fs.existsSync() 함수를 사용하여 dir 경로에 해당하는 폴더나 파일이 존재하지 않는지 확인합니다. 
+// "!" 연산자는 함수가 반환하는 값을 불리언으로 바꾸어, 폴더가 존재하지 않는 경우 true로 변환됩니다.
+// fs.mkdirSync(dir);: fs.mkdirSync() 함수를 사용하여 dir 경로에 해당하는 폴더를 생성합니다.
+//mkdirSync() 함수는 동기적으로 실행되어, 폴더를 생성하고 나서야 다음 코드로 진행합니다.
+//아래코드는 현재 작업 디렉토리에 result라는 이름의 폴더가 없는 경우, 해당 폴더를 생성하는 기능을 수행한다.
         const dir = './result';
         if (!fs.existsSync(dir)) {
           fs.mkdirSync(dir);
